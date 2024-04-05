f988fab96e710eec9b76143581ec5e45e233589e
naimisha15 <111036742+naimisha15@users.noreply.github.com>
2024-04-05 13:13:56 +0530

    Add files via upload

---
+++ apps.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.apps import AppConfig
+
+
+class ResearchSiteConfig(AppConfig):
+    name = 'Service_Provider'
+from django.db import models
+from django.test import TestCase
